bfad20c1b4922f76a6011a1cf3ba23eb5ad35c56
sigma390 <sigmacoder390@gmail.com>
2026-09-08 21:08:21 +0530

    feat: add post retrieval endpoints with ID-based error handling

--- app/app.py
+++ app/app.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 
 
 app = FastAPI()
 
+text_posts = {
+    1: {
+        "title": "My first post",
+        "content": "Hello from FastAPI!"
+    },
+    2: {
+        "title": "Learning FastAPI",
+        "content": "FastAPI is great for building APIs."
+    },
+    3: {
+        "title": "Python",
+        "content": "Python makes backend development fun."
+    }
+}
 
 
-@app.get('/hello')
-def hello():
-    return {"messsage":"Hello here"}#json , hence we return either a dict or a Pydantic Object
+@app.get("/posts")
+def get_posts():
+    return text_posts
+
+
+@app.get("/posts/{id}")
+def get_post_by_id(id:int):
+    if id not in text_posts:
+        return HTTPException(status_code=404 , detail="Post Not Found" )
+    return text_posts[id]
