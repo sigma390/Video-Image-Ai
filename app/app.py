1ffef2c4f57033d96da390c1c763a3cd8ddd5b62
sigma390 <sigmacoder390@gmail.com>
2026-09-09 18:37:15 +0530

    feat: add /feed endpoint to retrieve and serialize all posts from the database

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, File, Form, UploadFile, Depends  # fastapi tools
 from app.schemas import PostResponse, PostCreate                             # pydantic schemas
 from app.db import Post, create_tables, get_async_session                    # db models & session
+from sqlalchemy import select                                                # sql query builder
 # pyrefly: ignore [missing-import]
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
@@ -28,3 +29,21 @@ async def upload_file(
     await session.commit()       # save to db
     await session.refresh(post)  # refresh to get new id
     return post                  # return created post
+
+@app.get("/feed")
+async def get_feed(
+    session: AsyncSession = Depends(get_async_session)  # db session
+):
+    result = await session.execute(select(Post))  # query all posts
+    posts = result.scalars().all()                # get post list
+    post_data = []
+    for post in posts:
+        post_data.append({
+            "id":str(post.id),
+            "caption": post.caption,
+            "file_name": post.file_name,
+            "file_type": post.file_type,
+            "url": post.url,
+            "created_at": post.created_at
+        })
+    return post_data
