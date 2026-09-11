d9388f7a77298ac717aea9bc22253d0ae3ea43ba
sigma390 <sigmacoder390@gmail.com>
2026-09-11 22:16:53 +0530

    feat: implement admin authorization middleware and restrict user deletion to admin users

--- app/auth.py
+++ app/auth.py
@@ -75,3 +75,15 @@ async def get_current_user(
         raise credentials_exception
     return user
     
+
+async def require_admin(
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role!="admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access Required",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return current_user
--- app/router/user.py
+++ app/router/user.py
@@ -1,9 +1,11 @@
+from sqlalchemy import null
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from fastapi import HTTPException
 from app.db import User, get_async_session
 from app.schemas import UserResponse
+from app.auth import require_admin
 import uuid
 router = APIRouter(
     prefix="/users",
@@ -18,11 +20,15 @@ async def get_all_users(session: AsyncSession = Depends(get_async_session)) -> l
     return users_list
 
 @router.delete("/{user_id}")
-async def delete_user_by_id(user_id: str, session: AsyncSession = Depends(get_async_session)):
+async def delete_user_by_id(user_id: str, session: AsyncSession = Depends(get_async_session), admin=Depends(require_admin)):
     result = await session.execute(select(User).where(User.id == uuid.UUID(user_id)))  # find user
     user = result.scalars().first()                                                     # get user
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     await session.delete(user)
     await session.commit()
-    return {"message": "User deleted successfully"}    
+    return {"message": "User deleted successfully"} 
+
+
+    
+       
